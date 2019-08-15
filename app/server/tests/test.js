4b92fca8c635b78f9ada004cdb074b8460191383
Jason Owens <jasonaowens17@gmail.com>
2019-08-15 15:24:41 -0400

    Testing verification emails

--- app/server/tests/test.js
+++ app/server/tests/test.js
@@ -1,18 +1,19 @@
-// Initalize server instance for testing
-var server = require("../../../app");
+require("dotenv").load({ silent: true });
 
-// Populate env with values for testing
-// process.env.NODE_ENV = "test";
-
+// Inject testing env
+process.env.NODE_ENV = "test";
 process.env.EMAIL_HOST = process.env.TEST_EMAIL_HOST;
 process.env.EMAIL_USER = process.env.TEST_EMAIL_USER;
 process.env.EMAIL_PASS = process.env.TEST_EMAIL_PASS;
 process.env.EMAIL_PORT = process.env.TEST_EMAIL_PORT;
 process.env.EMAIL_SECURE = process.env.TEST_EMAIL_SECURE;
+// Initalize server instance for testing
+var server = require("../../../app");
+
+let runUserUnitTest = require("./unit-tests/auth-test");
 
 //------------//
 // Unit Tests //
 //------------//
-let runUserUnitTest = require("./unit-tests/auth-test");
 
 runUserUnitTest(server);
